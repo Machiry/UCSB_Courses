2bc8bbe54d0102d98454f8decb2d01db78e3e4a2
machiry <machiry_msidc@hotmail.com>
2015-05-05 00:56:12 -0700

    Adding CRC code

---
+++ Spring2015/CS290g/hw3/code/CRC.py
@@ -0,0 +1,52 @@
+import random
+import sys
+
+'''
+    Extended Euclid algorithm.
+'''
+def eea(n,r):
+    g0 = r
+    g1 = n
+    u0 = 1
+    u1 = 0
+    v0 = 0
+    v1 = 1
+    while not (g1 == 0):
+        q = g0 / g1
+        n_g0 = g1
+        n_g1 = g0 - g1*q
+        n_u0 = u1
+        n_u1 = u0 - u1*q
+        n_v0 = v1
+        n_v1 = v0 - v1*q
+        u0 = n_u0
+        u1 = n_u1
+        v0 = n_v0
+        v1 = n_v1
+        g0 = n_g0
+        g1 = n_g1
+        
+    return (g0,u0,v0)
+        
+    
+m = 50
+p = 97
+q = 103
+N = 9991
+phiN = 9792
+e = 2015
+d = 8927
+
+d1 = d%(p-1)
+d2 = d%(q-1)
+
+(modulus,q1,p1) = eea(p,q)
+
+assert(modulus == 1)
+
+m1 = (m**d1)%p
+m2 = (m**d2)%q
+s= m1 + p*(((m2-m1)*p1)%q)
+print 'Answer:' + str(s)
+   
+    
