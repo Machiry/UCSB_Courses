ee388a98e96c0cf130ff59424e03ddd9229ab858
machiry <machiry_msidc@hotmail.com>
2015-06-02 17:34:50 -0700

    Modified HW5

--- Spring2015/CS290g/hw5/code/compute_all_ele.py
+++ Spring2015/CS290g/hw5/code/compute_all_ele.py
@@ -8,28 +8,56 @@ def is_square(apositiveint):
         if x in seen: return False
         seen.add(x)
     return True
- 
+
+def get_residue_point(target,mod):
+    sol = (target**((mod-1)/2))%mod
+    # check if its a quadratic residue
+    if sol == 1:
+        # if yes, 
+        #assert((mod+1)%4 == 0)
+        return (target**((mod+1)/4))%mod
+    else:
+        return None
+
 print_s = ''
-for i in range(29):
+mod = 29
+order = 0
+for i in range(mod):
     print_s = str(i) + ' & '
     y = i*i*i - 3*i + 4
-    y = y % 29
+    #y = i*i*i + i + 1
+    y = y % mod
     print_s += str(y) + ' & '
     y_s = None
     if y == 0 or y == 1:
         y_s = y
-        print_s += '$\pm'+str(y_s)+'$ & '
+        if y == 1:        
+            print_s += '$\pm'+str(y_s)+'$ & '
+        else:
+            print_s += '0 &'
     elif is_square(y):
         y_s = int(math.sqrt(y))
-        y_s = y_s % 29
+        y_s = y_s % mod
         print_s += '$\pm'+str(y_s)+'$ & '
     else:
-        print_s += '- & '
+        resi = get_residue_point(y, mod)
+        if resi is None:
+            print_s += '- & '
+        else:
+            y_s = resi
+            print_s += '$\pm'+str(y_s) +'$ & '
     if y_s is not None:
-        print_s += '('+str(i)+','+str(y_s)+'), (' + str(i) + ',' + str(29-y_s) +') \\\\' + '\n'
+        if y_s > 0:
+            order += 2
+            print_s += '('+str(i)+','+str(y_s)+'), (' + str(i) + ',' + str(mod-y_s) +') \\\\' + '\n'
+        else:
+            order += 1
+            print_s += '('+str(i)+','+str(y_s)+') \\\\' + '\n'
     else:
         print_s += ' - \\\\\n'
     print_s += '\\hline'
     print print_s
+
+print '\n\n Order of ECG:' + str(order+1)
         
     
